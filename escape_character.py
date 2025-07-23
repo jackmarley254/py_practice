b6e9dd9f6f72bef879ba85001caf468c9418d229
jackmarley254 <engndiritu1@gmail.com>
2025-07-23 09:39:29 +0300

    Estimate values

--- escape_character.py
+++ escape_character.py
@@ -19,7 +19,8 @@ print(txt)
 
 #A backslash followed by three integers will result in a octal value:
 txt = "\110\145\154\154\157"
-print(txt) 
+print(txt)
+ 
 txt = "\x48\x65\x6c\x6c\x6f"
 print(txt)
 
---
+++ estimate_values.py
@@ -0,0 +1,35 @@
+print(bool("Hello"))
+print(bool(""))  # Output: False
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+class myclass():
+  def __len__(self):
+    return 0
+
+myobj = myclass()
+print(bool(myobj))
+
+def myFunction() :
+  return True
+
+print(myFunction())
+
+x = 200
+print(isinstance(x, int))
