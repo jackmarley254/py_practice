39a5b5090f448866b46062d8768684a16a963ac0
jackmarley254 <engndiritu1@gmail.com>
2025-07-24 21:13:58 +0300

    python collections-lists

---
+++ lists.py
@@ -0,0 +1,15 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+list1 = ["abc", 34, True, 40, "male"]
+print(len(list1))
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+thislist = list(("apple", "banana", "cherry")) #note the double
+#round-brackets
+print(thislist)
