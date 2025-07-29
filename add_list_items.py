7b128ad1f0c683e517d3ef7147ed03c67557db9e
jackmarley254 <engndiritu1@gmail.com>
2025-07-29 22:55:52 +0300

    extend list

--- add_list_items.py
+++ add_list_items.py
@@ -5,3 +5,8 @@ print(thislist)
 thislist = ["apple", "banana", "cherry"]
 thislist.insert(1, "orange")
 print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
