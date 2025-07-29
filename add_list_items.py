92d7bbf0a3669ac3d7934ac74dac0e1b18671035
jackmarley254 <engndiritu1@gmail.com>
2025-07-29 22:57:32 +0300

    add any iterable object

--- add_list_items.py
+++ add_list_items.py
@@ -10,3 +10,8 @@ thislist = ["apple", "banana", "cherry"]
 tropical = ["mango", "pineapple", "papaya"]
 thislist.extend(tropical)
 print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
