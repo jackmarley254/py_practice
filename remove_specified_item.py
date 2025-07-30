3ad89cc985562cb86e911c98f972bfbe352fae7d
jackmarley254 <engndiritu1@gmail.com>
2025-07-30 08:30:43 +0300

    remove specified items from a list

---
+++ remove_specified_item.py
@@ -0,0 +1,28 @@
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+thislist.remove("banana")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+#deletes list completely
+thislist = ["apple", "banana", "cherry"]
+del thislist
+
+#empties the list
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
