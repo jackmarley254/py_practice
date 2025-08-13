02cfd51005f74d1557c82a70d499d38be30c0642
jackmarley254 <engndiritu1@gmail.com>
2025-08-13 14:22:04 +0300

    Access tuples by index

---
+++ access_tuple.py
@@ -0,0 +1,9 @@
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-1])
+
+#index 2 (included) and end at index 5 (not included)
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
