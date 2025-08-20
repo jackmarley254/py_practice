756e9c635ebd0db7f655f21857c48ca948ba7599
jackmarley254 <engndiritu1@gmail.com>
2025-08-20 18:38:38 +0300

    Add tuple to a tuple

--- update_tuple.py
+++ update_tuple.py
@@ -10,3 +10,10 @@ thistuple = ("apple", "banana", "cherry")
 y = list(thistuple)
 y.append("orange")
 thistuple = tuple(y)
+
+#Add tuple to a tuple
+thistuple = ("apple", "banana", "cherry")
+y = ("orange",)
+thistuple += y
+
+print(thistuple)
