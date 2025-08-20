49b59821f3fca999f1a8928bef4144c8fb3feeac
jackmarley254 <engndiritu1@gmail.com>
2025-08-20 18:36:59 +0300

    tupples workound

--- update_tuple.py
+++ update_tuple.py
@@ -1,6 +1,12 @@
+#Convert the tuple into a list, add "orange", and convert it back into a tuple:
 x = ("apple", "banana", "cherry")
 y = list(x)
 y[1] = "kiwi"
 x = tuple(y)
 
 print(x)
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
